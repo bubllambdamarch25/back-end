558f53ea98ddb1054dc3a991151c820a2f6aeb38
wcolts2000 <wcolts2000@gmail.com>
2019-03-25 15:06:00 -0700

    testing deployment with sqlite3 db

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,57 @@
 // Update with your config settings.
 
 const prodDbConnection = {
-  host: 'localhost',
-  database: 'bublDB',
-  user: 'student',
-  password: 'password'
-}
+  host: "localhost",
+  database: "bublDB",
+  user: "student",
+  password: "password"
+};
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './data/bublDB.sqlite3'
+      filename: "./data/bublDB.sqlite3"
     },
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
     }
   },
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './data/testingDB.sqlite3'
+      filename: "./data/testingDB.sqlite3"
     },
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
     }
   },
+  // production: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL || prodDbConnection,
+  //   migrations: {
+  //     directory: './data/migrations'
+  //   },
+  //   seeds: {
+  //     directory: './data/seeds'
+  //   }
+  // }
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL || prodDbConnection,
+    client: "sqlite3",
+    connection: "./data/testingDB.sqlite3",
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
     }
   }
 };
